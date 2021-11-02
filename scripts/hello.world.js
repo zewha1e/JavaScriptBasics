433ffd95336044f4e2d2d55020626c73eda0b281
garciaam2 <garciaam2@penguin.com>
2021-11-02 15:49:53 -0600

    Adding veriable file

---
+++ scripts/hello.world.js
@@ -0,0 +1,13 @@
+// this is a comment
+// this is my hello world program
+/*
+
+
+this is a block comment
+
+*/
+
+let firstName = "zewhale";
+let age = 50;
+
+console.log("hello.worls my name is %s and i am %d years old.", firstName, age);
---
+++ scripts/scrpts.js
@@ -0,0 +1,9 @@
+
+// variables are named containers for stuff
+
+// let is a keyword to declare varibles
+// age is the name of my varible
+// 16 is the value assigned to varible age
+let myvarible = 16; // the data type of age is a number 
+
+let name = "zewhale";
